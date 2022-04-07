cb55c1873ede9e32f0496a899caa7e6c46548e5e
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-04-08 02:36:35 +0900

    Update 10757.py

--- 10757.py
+++ 10757.py
@@ -11,25 +11,38 @@ if __name__ == "__main__":
         temp = a
         a = b
         b = temp
+    elif len(a) == len(b):
+        if a[0] < b[0]:
+            temp = a
+            a = b
+            b = temp
 
     result = []
-    for i in reversed(range(len(a))):
-        sum = a[i] + b[i]
+    diff = len(a) - len(b)
+    for i in reversed(range(len(b))):
+        sum = a[i + diff] + b[i]
         if sum >= 10:
-            sum -= 10
-            if i > 0:
-                result.append(sum)
-                a[i - 1] += 1
+            if i == 0 and diff > 0:
+                a[i + diff] = sum - 10
+                a[i + diff - 1] += 1
+            elif i == 0 and diff == 0:
+                a[i + diff] = sum
             else:
-                result.append(sum)
-                result.append(1)
+                a[i + diff] = sum - 10
+                a[i + diff - 1] += 1
         else:
-            result.append(sum)
+            a[i + diff] = sum
 
-    for i in reversed(range(len(a) - len(b))):
-        if a[i] >= 10:
-            a[i - 1] += 1
-        result.append(a[i])
+    # for i in reversed(range(diff)):
+    #     if a[i] >= 10:
+    #         if i == 0:
+    #             result.append(a[i] - 10)
+    #             result.append(1)
+    #         else:
+    #             result.append(a[i] - 10)
+    #             a[i - 1] += 1
+    #     else:
+    #         result.append(a[i])
 
-    for i in result[::-1]:
+    for i in a:
         print(i, end="")
