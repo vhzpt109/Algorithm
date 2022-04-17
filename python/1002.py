e8660972c55dbc83a4f92677c8588003e4742fe4
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-04-17 23:09:39 +0900

    2022/04/17 Upload

---
+++ python/1002.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    t = int(input())
+
+    for _ in range(t):
+        x1, y1, r1, x2, y2, r2 = map(int, input().split())
+        
---
+++ python/3053.py
@@ -0,0 +1,10 @@
+import math
+
+if __name__ == "__main__":
+    r = int(input())
+
+    EG_area = r ** 2 * math.pi
+    TG_area = r ** 2 * 2
+
+    print(EG_area)
+    print(TG_area)
