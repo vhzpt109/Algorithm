f9e485dc8028149339521553f575dec41036cf50
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-04-20 00:58:39 +0900

    2022/04/20 Upload

--- python/1018.py
+++ python/1018.py
@@ -6,14 +6,30 @@ if __name__ == "__main__":
     for _ in range(n):
         list_nm.append(list(input()))
 
-    cnt = 0
-    for i in range(n - 7):
-        for j in range(m - 7):
-            chess_board = list_nm[0][0:8]
+    min_repaint_cnt = 64
+    for row in range(n - 7):
+        for col in range(m - 7):
+            chess_board = []
+            for y in range(row, row + 8):
+                chess_board.append(list_nm[y][col:col + 8])
 
-            repaint_black = 0
-            repaint_white = 0
-            for y in range(8):
-                for x in range(8):
-                    if chess_board[x][y] == 'W':
-                        cnt += 1
+            chess_line_rule1 = list("WBWBWBWB")
+            chess_line_rule2 = list("BWBWBWBW")
+            sum_repaint_case1 = 0
+            sum_repaint_case2 = 0
+            for i in range(len(chess_board)):
+                if i % 2 == 0:
+                    sum_repaint_case1 += 8 - sum([have_chess_board_line == chess_line for have_chess_board_line, chess_line in
+                                          zip(chess_board[i], chess_line_rule1)])
+                    sum_repaint_case2 += 8 - sum([have_chess_board_line == chess_line for have_chess_board_line, chess_line in
+                                          zip(chess_board[i], chess_line_rule2)])
+                else:
+                    sum_repaint_case1 += 8 - sum([have_chess_board_line == chess_line for have_chess_board_line, chess_line in
+                                          zip(chess_board[i], chess_line_rule2)])
+                    sum_repaint_case2 += 8 - sum([have_chess_board_line == chess_line for have_chess_board_line, chess_line in
+                                          zip(chess_board[i], chess_line_rule1)])
+
+            if min(sum_repaint_case1, sum_repaint_case2) < min_repaint_cnt:
+                min_repaint_cnt = min(sum_repaint_case1, sum_repaint_case2)
+
+    print(min_repaint_cnt)
--- python/2751.py
+++ python/2751.py
@@ -1,10 +1,12 @@
+import sys
+
 if __name__ == "__main__":
-    n = int(input())
+    n = int(sys.stdin.readline())
 
     list = []
 
     for _ in range(n):
-        list.append(int(input()))
+        list.append(int(sys.stdin.readline()))
     list.sort()
 
     for number in list:
