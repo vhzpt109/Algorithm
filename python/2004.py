cc34bb2daa0fa84fc79285a00119113c8f854b1f
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-05-24 20:53:42 +0900

    2022/05/24 Upload

--- python/2004.py
+++ python/2004.py
@@ -1,28 +1,22 @@
 def two_cnt(n):
     cnt = 0
-    for i in range(1, n):
-        if i % 2 == 0:
-            cnt += 1
+    while n != 0:
+        n = n // 2
+        cnt += n
     return cnt
 
 def five_cnt(n):
     cnt = 0
-    for i in range(1, n):
-        if i % 5 == 0:
-            cnt += 1
+    while n != 0:
+        n = n // 5
+        cnt += n
     return cnt
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
 
-    n_two = two_cnt(n)
-    n_five = five_cnt(n)
+    n_two = two_cnt(n) - two_cnt(m) - two_cnt(n - m)
+    n_five = five_cnt(n) - five_cnt(m) - five_cnt(n - m)
 
-    m_two = two_cnt(m)
-    m_five = five_cnt(m)
-
-    n_m_two = two_cnt(n - m)
-    n_m_five = five_cnt(n - m)
-
-    result = n_two + m
+    print(min(n_two, n_five))
