f2ea596e38e9e1deece62e324b04ffb74d4c0bc9
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-05-31 02:32:20 +0900

    2022/05/31 Upload

--- python/5430.py
+++ python/5430.py
@@ -1,42 +1,39 @@
+from collections import deque
+
 if __name__ == "__main__":
     t = int(input())
 
     for _ in range(t):
         p = list(input())
-        p_tuning = p[0]
-        count = 0
-        for func in p:
-            if func == p_tuning[-1]:
-                count += 1
-            else:
-                p_tuning += str(count) + func
-                count = 1
-        p_tuning += str(count)
-        p_tuning = list(p_tuning)
 
         n = int(input())
-        n_list = list(input()[1:-1].split(','))
+        n_list = deque(list(input()[1:-1].split(',')))
 
         error_check = False
-        for i in range(0, len(p_tuning), 2):
-            if p_tuning[i] == 'R':
-                if int(p_tuning[i + 1]) % 2 != 0:
-                    n_list.reverse()
-            elif p_tuning[i] == 'D':
-                for d_n in range(int(p_tuning[i + 1])):
-                    if n > 1:
-                        n_list.pop(0)
-                        n -= 1
+        reverse_flag = False
+        for func in p:
+            if func == 'R':
+                reverse_flag = not reverse_flag
+            elif func == 'D':
+                if n > 0:
+                    n -= 1
+                    if not reverse_flag:
+                        n_list.popleft()
                     else:
-                        error_check = True
-                        break
-            if error_check:
-                break
+                        n_list.pop()
+                else:
+                    error_check = True
 
         if not error_check:
             print("[", end="")
-            for n in n_list[:-1]:
-                print(n + ",", end="")
-            print(n_list[-1] + "]")
+            result_n_list = list(n_list)
+            if len(result_n_list) > 0:
+                if reverse_flag:
+                    result_n_list.reverse()
+                for n in result_n_list[:-1]:
+                    print(n + ",", end="")
+                print(result_n_list[-1] + "]")
+            else:
+                print("]")
         else:
             print("error")
