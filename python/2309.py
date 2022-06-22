6305a95b54ae296cf24c06c6e323205fc38b53d3
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-06-23 01:59:55 +0900

    2022/06/23 Upload

---
+++ python/2309.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+    n_list = []
+    for _ in range(9):
+        n_list.append(int(input()))
+
+    for i in range(8):
+        for j in range(i + 1, 9):
+            n_i = n_list[i]
+            n_j = n_list[j]
+            n_sum = sum(n_list) - n_i - n_j
+            if n_sum == 100:
+                n_list.remove(n_i)
+                n_list.remove(n_j)
+                break
+        if len(n_list) == 7:
+            break
+
+    n_list.sort()
+    for n in n_list:
+        print(n)
--- python/2579.py
+++ python/2579.py
@@ -13,31 +13,14 @@ if __name__ == "__main__":
         elif n_stairs == 2:
             print(stairs_list[1] + stairs_list[2])
         elif n_stairs == 3:
-            print(max(stairs_list[2] + stairs_list[3], stairs_list[1] + stairs_list[3]))
+            print(max(stairs_list[1] + stairs_list[3], stairs_list[2] + stairs_list[3]))
         exit()
 
     dp[1] = stairs_list[1]
     dp[2] = stairs_list[1] + stairs_list[2]
-    if max(stairs_list[2] + stairs_list[3], stairs_list[1] + stairs_list[3]) == stairs_list[1] + stairs_list[3]:
-        dp[3] = stairs_list[1] + stairs_list[3]
-    else:
-        dp[3] = stairs_list[2] + stairs_list[3]
-        near_count += 1
+    dp[3] = max(stairs_list[1] + stairs_list[3], stairs_list[2] + stairs_list[3])
 
-    for i in range(4, n_stairs):
-        if dp[i - 1] == dp[i - 2]:
-            dp[i] = dp[i - 2] + stairs_list[i]
-            near_count = 0
-        elif max(dp[i - 1], dp[i - 2]) == dp[i - 1] and near_count != 1:
-            dp[i] = dp[i - 1] + stairs_list[i]
-            near_count += 1
-        else:
-            dp[i] = dp[i - 2] + stairs_list[i]
-            near_count = 0
-
-    if max(dp[n_stairs - 1], dp[n_stairs - 2]) == dp[n_stairs - 1] and near_count != 1:
-        dp[n_stairs] = dp[n_stairs - 1] + stairs_list[n_stairs]
-    else:
-        dp[n_stairs] = dp[n_stairs - 2] + stairs_list[n_stairs]
+    for i in range(4, n_stairs + 1):
+        dp[i] = max(dp[i - 2] + stairs_list[i], dp[i - 3] + stairs_list[i - 1] + stairs_list[i])
 
     print(dp[n_stairs])
