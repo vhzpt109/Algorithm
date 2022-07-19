471c43a53776e42faf85567b72638acbb2632f78
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-07-20 01:25:57 +0900

    2022/07/20 Upload

--- python/1753.py
+++ python/1753.py
@@ -1,46 +1,38 @@
+import heapq
 import sys
 
 input = sys.stdin.readline
 INF = int(1e9)
 
 
-def get_smallest_node():
-    min_value= INF
-    index = 0
-    for i in range(1, v + 1):
-        if distance[i] < min_value and not visited[i]:
-            min_value = distance[i]
-            index = i
-    return index
-
 def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
     distance[start] = 0
-    visited[start] = True
-    for j in graph[start]:
-        distance[j[0]] = j[1]
-    for i in range(v - 1):
-        now = get_smallest_node()
-        visited[now] = True
-        for j in graph[now]:
-            cost = distance[now] + j[1]
-            if cost < distance[j[0]]:
-                distance[j[0]] = cost
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
 
 if __name__ == "__main__":
-    v, e = map(int, input().split())
+    n, m = map(int, input().split())
     k = int(input())
-    graph = [[] for i in range(v + 1)]
-    visited = [False] * (v + 1)
-    distance = [INF] * (v + 1)
+    graph = [[] for i in range(n + 1)]
+    distance = [INF] * (n + 1)
 
-    for _ in range(e):
+    for _ in range(m):
         u, v, w = map(int, input().split())
         graph[u].append((v, w))
 
     dijkstra(k)
 
-    for i in range(1, v + 1):
+    for i in range(1, n + 1):
         if distance[i] == INF:
             print("INF")
         else:
