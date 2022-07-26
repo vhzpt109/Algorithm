900f4be5653f873cbff06b87c186385bc1c64321
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-07-26 22:37:34 +0900

    2022/07/26 Upload

--- python/5582.py
+++ python/5582.py
@@ -1,16 +1,15 @@
 if __name__ == "__main__":
-    s1 = list(input())
-    s2 = list(input())
+    s1 = input().rstrip()
+    s2 = input().rstrip()
 
-    dp = [[0] * (len(s1)) for _ in range(len(s2))]
+    dp = [[0] * len(s2) for _ in range(len(s1))]
 
-    for i in range(len(s2)):
-        for j in range(len(s1)):
-            if i == 0 or j == 0:
-                dp[i][j] = 0
-            elif s1[j] == s2[i]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = 0
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
 
     print(max(map(max, dp)))
