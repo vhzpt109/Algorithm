a6f16f4aee5eb93edcd3781e67a6d6bc9e5c7462
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-07-27 23:22:38 +0900

    2022/07/27 Upload

--- python/17298.py
+++ python/17298.py
@@ -1,18 +1,16 @@
 import sys
 
 
-def NGE(A, Ai):
-    for a in A:
-        if a > Ai:
-            return a
-    else:
-        return -1
-
-
 if __name__ == "__main__":
     n = int(sys.stdin.readline())
-
     A = list(map(int, sys.stdin.readline().split()))
 
-    for i in range(n):
-        print(NGE(A[i + 1:], A[i]), end=" ")
+    stack = []
+    result = [-1 for i in range(n)]
+
+    for i in range(len(A)):
+        while stack and A[stack[-1]] < A[i]:
+            result[stack.pop()] = A[i]
+        stack.append(i)
+
+    print(*result)
