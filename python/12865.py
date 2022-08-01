dda38fbbb9a6fc8b65a5410da5204c8171864f5b
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-08-02 02:09:21 +0900

    2022/08/02 Upload

--- python/12865.py
+++ python/12865.py
@@ -4,14 +4,21 @@ input = sys.stdin.readline
 
 if __name__ == "__main__":
     n, k = map(int, input().split())
-    dp = [[0] * (n + 1) for _ in range(k + 1)]
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    belongings = [[0, 0]]
 
-    for j in range(n):
+    for _ in range(n):
         w, v = map(int, input().split())
-        for i in range(w):
-            if w + i <= k:
-                dp[i][j] = max(dp[i][j - 1], dp[i][j - 1] + v)
+        belongings.append((w, v))
+
+    for i in range(1, n + 1):
+        for j in range(1, k + 1):
+            weight = belongings[i][0]
+            value = belongings[i][1]
+
+            if j < weight:
+                dp[i][j] = dp[i - 1][j]
             else:
-                dp[i][j] = dp[i][j - 1]
-    de = 10
-    print(dp[k][n])
+                dp[i][j] = max(dp[i - 1][j - weight] + value, dp[i - 1][j])
+
+    print(dp[n][k])
