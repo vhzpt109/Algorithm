d6ffe0cdf94eb9e61d020d50a5774f7bd887ba2f
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-08-05 01:23:42 +0900

    2022/08/05 Upload

--- python/11660.py
+++ python/11660.py
@@ -5,20 +5,9 @@ input = sys.stdin.readline
 if __name__ == "__main__":
     n, m = map(int, input().split())
 
-    graph = []
-    for _ in range(n):
-        graph.append(list(map(int, input().split())))
+    graph = [list(map(int, input().split())) for _ in range(n)]
 
-    sum_list = []
-    for y in range(n):
-        sub_sum_list = [0, graph[y][0]]
+    graph_sum = [[0] * (n + 1) for _ in range(n + 1)]
+    for y in range(1, n):
         for x in range(1, n):
-            sub_sum_list.append(sub_sum_list[x] + graph[y][x])
-        sum_list.append(sub_sum_list)
-
-    for _ in range(m):
-        y1, x1, y2, x2 = map(int, input().split())
-        result = 0
-        for y in range(y1, y2 + 1):
-            result += sum_list[y - 1][x2] - sum_list[y - 1][x1 - 1]
-        print(result)
+            graph_sum[y][x] = graph_sum[y - 1][x] + graph_sum[y][x - 1] + graph[y][x]
