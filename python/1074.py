8a0bedd27776a2c95795cfb15efe7b9b8042fb4d
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-08-10 21:32:44 +0900

    2022/08/10 Upload

--- python/1074.py
+++ python/1074.py
@@ -1,26 +1,21 @@
-number = 0
-
-
-def Z(array, x, y, n):
-    global number
-    if n != 2:
-        Z(array, x, y, n // 2)
-        Z(array, x + n // 2, y, n // 2)
-        Z(array, x, y + n // 2, n // 2)
-        Z(array, x + n // 2, y + n // 2, n // 2)
-    else:
-        array[y][x] = number
-        array[y][x + 1] = number + 1
-        array[y + 1][x] = number + 2
-        array[y + 1][x + 1] = number + 3
-        number += 4
-
-
 if __name__ == "__main__":
     n, r, c = map(int, input().split())
 
-    array = [[0] * 2 ** n for _ in range(2 ** n)]
+    result = 0
 
-    Z(array, 0, 0, 2**n)
+    while n != 0:
+        n -= 1
+        if r < 2**n and c < 2**n:
+            result += 2**n * 2**n * 0
+        elif r < 2**n <= c:
+            result += 2**n * 2**n * 1
+            c -= 2 ** n
+        elif r >= 2**n > c:
+            result += 2**n * 2**n * 2
+            r -= 2**n
+        else:
+            result += 2**n * 2**n * 3
+            r -= 2**n
+            c -= 2**n
 
-    print(array[r][c])
+    print(result)
