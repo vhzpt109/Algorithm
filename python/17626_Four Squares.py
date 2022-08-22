96c20097fa0d43cf8c20eeef26ec43f7fad5434a
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-08-23 02:08:34 +0900

    2022/08/23 Upload

--- python/17626_Four Squares.py
+++ python/17626_Four Squares.py
@@ -3,12 +3,12 @@ import math
 if __name__ == "__main__":
     n = int(input())
 
-    count = 0
-    while n > 0:
-        if math.sqrt(n) > 1:
-            n -= int(math.sqrt(n))**2
-            count += 1
-        else:
-            count += 1
-            break
-    print(count)
+    dp = [0] * (n + 1)
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        minval = 1e9
+        for j in range(1, int(math.sqrt(i)) + 1):
+            minval = min(minval, dp[i - (j**2)])
+        dp[i] = minval + 1
+    print(dp[n])
