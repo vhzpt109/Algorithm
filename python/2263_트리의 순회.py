904d92a78afb451c68550ca95aeb2e0ffb8e150e
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-08-28 18:24:02 +0900

    TreeTraversal

---
+++ python/2263_트리의 순회.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+input = sys.stdin.readline
+
+
+def preorder(inorderStart, inorderEnd, postorderStart, postorderEnd):
+    if (inorderStart > inorderEnd) or (postorderStart > postorderEnd):
+        return
+
+    root = postorder[postorderEnd]
+
+    left = node[root] - inorderStart
+    right = inorderEnd - node[root]
+
+    print(root, end=" ")
+    preorder(inorderStart, inorderStart + left - 1, postorderStart, postorderStart + left - 1)
+    preorder(inorderEnd - right + 1, inorderEnd, postorderEnd - right, postorderEnd - 1)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    inorder = list(map(int, input().split()))
+    postorder = list(map(int, input().split()))
+
+    node = [0] * (n + 1)
+    for i in range(n):
+        node[inorder[i]] = i
+
+    preorder(0, n - 1, 0, n - 1)
