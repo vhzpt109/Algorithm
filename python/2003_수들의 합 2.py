389c80a29fd3591bdfb25b7cd12343b04376f111
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-08-30 00:53:45 +0900

    two point

--- python/2003_수들의 합 2.py
+++ python/2003_수들의 합 2.py
@@ -2,15 +2,15 @@ if __name__ == "__main__":
     n, m = map(int, input().split())
     A = list(map(int, input().split()))
 
-    prefix_sum_A_list = [0, A[0], A[0] + A[1]]
+    prefix_sum_A_list = [0, A[0]]
 
-    for i in range(2, n):
+    for i in range(1, n):
         prefix_sum_A_list.append(prefix_sum_A_list[i] + A[i])
 
     start, end = 0, 0
     count = 0
-    while start <= end and start <= n:
-        sum_two_point = sum(prefix_sum_A_list[start:end])
+    while start <= end and end <= n:
+        sum_two_point = prefix_sum_A_list[end] - prefix_sum_A_list[start]
         if sum_two_point == m:
             count += 1
             start += 1
