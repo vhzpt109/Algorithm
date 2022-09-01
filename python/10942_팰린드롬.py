f61a77bb5d2e52a121bd4d5c3385b7486a3112d8
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-09-02 01:37:22 +0900

    Dynamic Programing

--- python/10942_팰린드롬.py
+++ python/10942_팰린드롬.py
@@ -3,14 +3,24 @@ import sys
 input = sys.stdin.readline
 
 if __name__ == "__main__":
-    n = int(input().rstrip())
-    n_list = list(map(int, input().rstrip().split()))
+    n = int(input())
+    n_list = list(map(int, input().split()))
 
-    m = int(input().rstrip())
+    dp = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = 1
+
+    for i in range(n - 1):
+        if n_list[i] == n_list[i + 1]:
+            dp[i][i + 1] = 1
+
+    for i in range(2, n):
+        for j in range(n - i):
+            if n_list[j] == n_list[i + j] and dp[j + 1][i + j - 1] == 1:
+                dp[j][i + j] = 1
+
+    m = int(input())
     for _ in range(m):
-        s, e = map(int, input(),rstrip().split())
-        sub_string = n_list[s - 1:e]
-        if sub_string == sub_string[::-1]:
-            print(1)
-        else:
-            print(0)
+        s, e = map(int, input().split())
+        print(dp[s - 1][e - 1])
