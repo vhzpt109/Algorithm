0afae1a324326dc232dafc39b7404689349f2c88
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-10-01 00:51:20 +0900

    Recursion & Merge Algorithm

--- BOJ/24060_알고리즘 수업 - 병합 정렬 1.py
+++ BOJ/24060_알고리즘 수업 - 병합 정렬 1.py
@@ -1,38 +1,47 @@
-count = 0
+import sys
+
+input = sys.stdin.readline
+
 
 def merge_sort(arr):
-    global count
-    count += 1
-
-    if count == k:
-        print(arr)
-
-    if len(arr) < 2:
+    if len(arr) == 1:
         return arr
 
-    mid = len(arr) // 2
-    low_arr = merge_sort(arr[:mid])
-    high_arr = merge_sort(arr[mid:])
+    mid = (len(arr) + 1) // 2
 
-    merged_arr = []
-    l = h = 0
-    while l < len(low_arr) and h < len(high_arr):
-        if low_arr[l] < high_arr[h]:
-            merged_arr.append(low_arr[l])
-            l += 1
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    i = j = 0
+    temp = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            temp.append(left[i])
+            result.append(left[i])
+            i += 1
         else:
-            merged_arr.append(high_arr[h])
-            h += 1
-    merged_arr += low_arr[l:]
-    merged_arr += high_arr[h:]
-    return merged_arr
+            temp.append(right[j])
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        temp.append(left[i])
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        temp.append(right[j])
+        result.append(right[j])
+        j += 1
+
+    return temp
 
 
 if __name__ == "__main__":
     n, k = map(int, input().split())
-    A_list = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
+    result = []
+    merge_sort(arr)
 
-    merge_sort(A_list)
-
-    if count < k:
+    if len(result) >= k:
+        print(result[k - 1])
+    else:
         print(-1)
