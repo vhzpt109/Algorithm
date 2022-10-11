a4d437d458240116960494895119915bd0c44461
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-10-11 22:25:04 +0900

    Fix with wrong approach(BFS) DFS

--- BOJ/1520_내리막.py
+++ BOJ/1520_내리막.py
@@ -1,31 +1,34 @@
-from collections import deque
+import sys
 
+sys.setrecursionlimit(10 ** 6)
 
-def bfs(map_array, start):
-    queue = deque()
-    queue.append((start[0], start[1]))
+def dfs(map_array, x, y):
+    if x == n - 1 and y == m - 1:
+        return 1
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            xx = x + dx[i]
-            yy = y + dy[i]
-            if xx < 0 or xx >= m or yy < 0 or yy >= n:
-                continue
-            if map_array[yy][xx] == 1 or map_array[yy][xx] == -1:
-                continue
-            elif map_array[yy][xx] == 0:
-                map_array[yy][xx] = map_array[y][x] + 1
-                queue.append((xx, yy))
+    if dp[y][x] != -1:
+        return dp[y][x]
+
+    dp[y][x] = 0
+    for i in range(4):
+        xx = x + dx[i]
+        yy = y + dy[i]
+        if xx < 0 or xx >= n or yy < 0 or yy >= m:
+            continue
+        if map_array[yy][xx] < map_array[y][x]:
+            dp[y][x] += dfs(map_array, xx, yy)
+    return dp[y][x]
 
 
 if __name__ == "__main__":
     m, n = map(int, input().split())
+    dp = [[-1] * n for _ in range(m)]
+
     map_array = []
     for _ in range(m):
         map_array.append(list(map(int, input().split())))
 
-    bfs(map_array, (0, 0))
+    print(dfs(map_array, 0, 0))
