4c0726b38f861325b0ab3853affb9465980662a1
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-10-12 21:42:48 +0900

    DP Algorithm Base Code
    I don't have time to write only the input module

---
+++ BOJ/11066_파일 합치기.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        k = int(input())
+        file_size = list(map(int, input().split()))
+        # write your code
---
+++ BOJ/4999_아.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    jaehwan = input().rstrip()
+    doctor = input().rstrip()
+
+    if len(jaehwan) >= len(doctor):
+        print("go")
+    else:
+        print("no")
