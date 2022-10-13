cb5be2b48ed6e5b21fb912b887d4bcdbdb121b55
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-10-13 13:02:22 +0900

    DP

--- BOJ/11066_파일 합치기.py
+++ BOJ/11066_파일 합치기.py
@@ -3,4 +3,16 @@ if __name__ == "__main__":
     for _ in range(t):
         k = int(input())
         file_size = list(map(int, input().split()))
-        # write your code
+        dp = [[0] * (k + 1) for _ in range(k + 1)]
+
+        for i in range(k - 1):
+            dp[i][i + 1] = file_size[i] + file_size[i + 1]
+            for j in range(i + 2, k):
+                dp[i][j] = dp[i][j - 1] + file_size[j]
+
+        for v in range(2, k):
+            for i in range(k - v):
+                j = i + v
+                dp[i][j] += min([dp[i][z] + dp[z + 1][j] for z in range(i, j)])
+
+        print(dp[0][k - 1])
