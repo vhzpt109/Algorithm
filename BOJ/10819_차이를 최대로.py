40d17935249df62418186585e6a18e8710233c0b
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-10-29 21:12:49 +0900

    BackTacking Algorithm

--- BOJ/10819_차이를 최대로.py
+++ BOJ/10819_차이를 최대로.py
@@ -1,6 +1,20 @@
+def backtracking(x):
+    if x == n:
+        result_list.append(sum(abs(m[temp[i + 1]] - m[temp[i]]) for i in range(n - 1)))
+        return
+
+    for i in range(n):
+        if i not in temp:
+            temp.append(i)
+            backtracking(x + 1)
+            temp.pop()
+
+
 if __name__ == "__main__":
     n = int(input())
-    n_list = list(map(int, input().split()))
-    n_list.sort()
+    m = list(map(int, input().split()))
+    result_list = []
+    temp = []
+    backtracking(0)
 
-    print(sum(n_list[n - 1 - i] - n_list[i] for i in range(n)))
+    print(max(result_list))
