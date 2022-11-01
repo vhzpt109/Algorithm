460bde7478414326e38fb95206e40697e9e7d38b
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-11-01 22:43:10 +0900

    Segment Tree

--- BOJ/12899_데이터 구조.py
+++ BOJ/12899_데이터 구조.py
@@ -1,16 +1,34 @@
 import sys
-import heapq
+sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
+RANGE = int(2e6)
+
+
+def search(start, end, node, k):
+    return
+
+
+def update(start, end, node, index):
+    if index < start or index > end:
+        return
+    else:
+        tree[node] += 1
+        if start == end:
+            return
+        else:
+            mid = (start + end) // 2
+            update(start, mid, node * 2, index)
+            update(mid + 1, end, node * 2 + 1, index)
+
+
 if __name__ == "__main__":
     n = int(input())
-    heap = []
-    count = 0
+
+    tree = [[-1] for _ in range(RANGE)]
     for _ in range(n):
         t, x = map(int, input().split())
         if t == 1:
-            heapq.heappush(heap, x)
+            update(0, RANGE, 1, x)
         else:
-            value = heapq.nsmallest(x, heap)[x - 1]
-            print(value)
-            heap.remove(value)
+            print(search(0, RANGE, 1, x))
