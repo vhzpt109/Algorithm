3f4e164948f9159daff7e8e36237e08ec750024c
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-11-02 22:36:37 +0900

    Segment Tree

--- BOJ/12899_데이터 구조.py
+++ BOJ/12899_데이터 구조.py
@@ -5,21 +5,29 @@ input = sys.stdin.readline
 RANGE = int(2e6)
 
 
-def search(start, end, node, k):
-    return
+def search(start, end, node, kth):
+    if start == end:
+        return start
+    else:
+        mid = (start + end) // 2
+        if kth <= tree[node * 2]:
+            return search(start, mid, node * 2, kth)
+        else:
+            return search(mid, end, node * 2 + 1, kth - tree[node * 2])
 
 
-def update(start, end, node, index):
+def update(start, end, node, index, value):
     if index < start or index > end:
         return
     else:
-        tree[node] += 1
         if start == end:
+            tree[node] += value
             return
         else:
             mid = (start + end) // 2
-            update(start, mid, node * 2, index)
-            update(mid + 1, end, node * 2 + 1, index)
+            update(start, mid, node * 2, index, value)
+            update(mid + 1, end, node * 2 + 1, index, value)
+            tree[node] = tree[node * 2] + tree[node * 2 + 1]
 
 
 if __name__ == "__main__":
@@ -29,6 +37,8 @@ if __name__ == "__main__":
     for _ in range(n):
         t, x = map(int, input().split())
         if t == 1:
-            update(0, RANGE, 1, x)
+            update(0, RANGE, 1, x, 1)
         else:
-            print(search(0, RANGE, 1, x))
+            kth = search(0, RANGE, 1, x)
+            update(0, RANGE, 1, kth, -1)
+            print(kth)
