2181efed7d4bf427d1113aaaeb12d87ad33e95eb
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-11-03 23:53:32 +0900

    Segment Tree

--- BOJ/12899_데이터 구조.py
+++ BOJ/12899_데이터 구조.py
@@ -3,6 +3,7 @@ sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 RANGE = int(2e6)
+print(RANGE)
 
 
 def search(start, end, node, kth):
@@ -13,21 +14,21 @@ def search(start, end, node, kth):
         if kth <= tree[node * 2]:
             return search(start, mid, node * 2, kth)
         else:
-            return search(mid, end, node * 2 + 1, kth - tree[node * 2])
+            return search(mid + 1, end, node * 2 + 1, kth - tree[node * 2])
 
 
 def update(start, end, node, index, value):
     if index < start or index > end:
         return
+    if start == end:
+        print(node)
+        tree[node] += value
+        return
     else:
-        if start == end:
-            tree[node] += value
-            return
-        else:
-            mid = (start + end) // 2
-            update(start, mid, node * 2, index, value)
-            update(mid + 1, end, node * 2 + 1, index, value)
-            tree[node] = tree[node * 2] + tree[node * 2 + 1]
+        mid = (start + end) // 2
+        update(start, mid, node * 2, index, value)
+        update(mid + 1, end, node * 2 + 1, index, value)
+        tree[node] = tree[node * 2] + tree[node * 2 + 1]
 
 
 if __name__ == "__main__":
