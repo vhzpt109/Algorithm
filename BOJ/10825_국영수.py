10b111086edeccec2121547b06e0b21f4c203ebc
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-11-07 20:38:47 +0900

    Sort

---
+++ BOJ/10825_국영수.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    n = int(input())
+
+    score_list = []
+    for _ in range(n):
+        name, kor, eng, math = map(str, input().split())
+        score_list.append([name, int(kor), int(eng), int(math)])
+
+    sorted_score_list = sorted(score_list, key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+    for score in sorted_score_list:
+        print(score[0])
