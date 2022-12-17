7456a0abc8c326b4c6af128032051d070c162d3c
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-12-17 23:00:15 +0900

    LIS Algorithm

--- BOJ/26481_수열과 쿼리 42.py
+++ BOJ/26481_수열과 쿼리 42.py
@@ -1,13 +1,16 @@
+import sys
+
+input = sys.stdin.readline
 if __name__ == "__main__":
     n, q = map(int, input().split())
     A = list(map(int, input().split()))
     for _ in range(q):
-        i, j = map(int, input().split())
-        A_sub = A[i:j + 1]
+        l, r = map(int, input().split())
+        A_sub = A[l - 1:r]
 
         dp = [1] * len(A_sub)
-        for a in range(len(A_sub)):
-            for b in range(a):
-                if A[a] > A[b]:
-                    dp[a] = max(dp[a], dp[b] + 1)
+        for k in range(len(A_sub)):
+            for i in range(k):
+                if A_sub[i] < A_sub[k]:
+                    dp[k] = max(dp[k], dp[i] + 1)
         print(max(dp))
