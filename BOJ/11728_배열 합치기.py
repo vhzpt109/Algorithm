94bf2b8bed77b3b9dcfd87b278885c7a49938042
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-03-18 23:26:20 +0900

    implementation

---
+++ BOJ/11728_배열 합치기.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a.extend(b)
+    a.sort()
+
+    print(*a)
