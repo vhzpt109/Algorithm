39c14e11acf79c58dc23930cf795c0de767808e5
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-03-22 01:03:39 +0900

    DFS

--- BOJ/1987_알파벳.py
+++ BOJ/1987_알파벳.py
@@ -1,28 +1,29 @@
-from string import ascii_uppercase
-import copy
+import sys
+input = sys.stdin.readline
 
 
 def dfs(board, alphabet_count, visited, x, y, count):
     visited[y][x] = True
-    alphabet_count[board[y][x]] = 0
+    alphabet_count[ord(board[y][x]) - 65] = 0
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
 
-    max_count = 0
+    max_count = count
     for i in range(4):
         xx = x + dx[i]
         yy = y + dy[i]
 
         if xx < 0 or xx >= c or yy < 0 or yy >= r:
             continue
-        if alphabet_count[board[yy][xx]] == 0:
+        if alphabet_count[ord(board[yy][xx]) - 65] == 0:
             continue
         if visited[yy][xx]:
             continue
 
-        clone_alphabet_count = copy.deepcopy(alphabet_count)
-        max_count = max(dfs(board[:], clone_alphabet_count, visited[:], xx, yy, count + 1), count)
+        max_count = max(dfs(board, alphabet_count, visited, xx, yy, count + 1), max_count)
+        visited[yy][xx] = False
+        alphabet_count[ord(board[yy][xx]) - 65] = 1
 
     return max_count
 
@@ -32,8 +33,6 @@ if __name__ == "__main__":
     board = [list(input().rstrip()) for _ in range(r)]
     visited = [[False for _ in range(c)] for _ in range(r)]
 
-    alphabet_count = {}
-    for alphabet in list(ascii_uppercase):
-        alphabet_count[alphabet] = 1
+    alphabet_count = [1 for _ in range(26)]
 
-    print(dfs(board[:], copy.deepcopy(alphabet_count), visited[:], 0, 0, 1))
+    print(dfs(board, alphabet_count, visited, 0, 0, 1))
