a66e095ebf15dabcfdafcbf9c5bcacb88bccd9d1
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-03 00:54:17 +0900

    Stack

--- BOJ/2504_괄호의 값.py
+++ BOJ/2504_괄호의 값.py
@@ -3,28 +3,36 @@ bracket_dict = {
     "[": "]"
 }
 
+bracket_dict_by_value = {
+    ")": "(",
+    "]": "["
+}
+
 bracket_number = {
     "(": 2,
     "[": 3
 }
 
 if __name__ == "__main__":
-    _string = input().rstrip()
+    _string = input()
 
     bracket_sum = 0
     bracket_sub_sum = 1
     stack = []
-    for char in _string:
-        if char in bracket_dict.keys():
-            stack.append(char)
-            bracket_sub_sum *= bracket_number[char]
-        elif char in bracket_dict.values():
-            value = stack.pop()
-            if char != bracket_dict[value]:
+    for i in range(len(_string)):
+        if _string[i] in bracket_dict.keys():
+            stack.append(_string[i])
+            bracket_sub_sum *= bracket_number[_string[i]]
+
+        elif _string[i] in bracket_dict.values():
+            if not stack or bracket_dict[stack[-1]] != _string[i]:
                 print(0)
                 exit(0)
-            else:
+            if _string[i - 1] == bracket_dict_by_value[_string[i]]:
                 bracket_sum += bracket_sub_sum
-                bracket_sub_sum //= bracket_number[value]
-
-    print(bracket_sum)
+            stack.pop()
+            bracket_sub_sum //= bracket_number[bracket_dict_by_value[_string[i]]]
+    if stack:
+        print(0)
+    else:
+        print(bracket_sum)
