9e2a32b37e7db625d669be420d696d3c10c53f56
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-06 01:08:00 +0900

    BFS

--- BOJ/2206_벽 부수고 이동하기.py
+++ BOJ/2206_벽 부수고 이동하기.py
@@ -3,14 +3,14 @@ import sys
 from collections import deque
 
 
-def bfs(_map, visited, x, y, z):
-    queue = deque([(x, y, z)])
-    visited[y][x][z] = 1
+def bfs(_map, visited, x, y, z, distance):
+    queue = deque([(x, y, z, distance)])
+    visited[y][x][z] = True
 
     while queue:
-        x, y, z = queue.popleft()
+        x, y, z, distance = queue.popleft()
         if x == m - 1 and y == n - 1:
-            return visited[y][x][z]
+            return distance
 
         dx = [0, 0, 1, -1]
         dy = [1, -1, 0, 0]
@@ -20,11 +20,11 @@ def bfs(_map, visited, x, y, z):
             if xx < 0 or xx >= m or yy < 0 or yy >= n:
                 continue
             if _map[yy][xx] == 1 and z == 0:
-                visited[yy][xx][1] = visited[y][x][0] + 1
-                queue.append((xx, yy, 1))
-            elif _map[yy][xx] == 0 and visited[yy][xx][z] == 0:
-                visited[yy][xx][z] = visited[y][x][z] + 1
-                queue.append((xx, yy, z))
+                visited[yy][xx][1] = True
+                queue.append((xx, yy, 1, distance + 1))
+            elif _map[yy][xx] == 0 and visited[yy][xx][z] is False:
+                visited[yy][xx][z] = True
+                queue.append((xx, yy, z, distance + 1))
 
     return -1
 
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     n, m = map(int, input().rstrip().split())
 
     _map = [list(map(int, input().rstrip())) for _ in range(n)]
-    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    visited = [[[False, False] for _ in range(m)] for _ in range(n)]
 
-    print(bfs(_map, visited, 0, 0, 0))
+    print(bfs(_map, visited, 0, 0, 0, 1))
