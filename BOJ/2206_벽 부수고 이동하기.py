89fc54708cadf82fe3b2e2c70b680cb808176112
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-05 22:47:37 +0900

    BFS

--- BOJ/2206_벽 부수고 이동하기.py
+++ BOJ/2206_벽 부수고 이동하기.py
@@ -1,31 +1,33 @@
 import sys
-import copy
 
 from collections import deque
 
 
-def bfs(_map, visited, x, y):
-    queue = deque([(x, y, 1)])
-    visited[y][x] = True
+def bfs(_map, visited, x, y, z):
+    queue = deque([(x, y, z)])
+    visited[x][y][z] = 1
 
     while queue:
-        x, y, distance = queue.popleft()
+        x, y, z = queue.popleft()
+        if x == n - 1 and y == m - 1:
+            return visited[x][y][z]
 
-        dx = [-1, 0, 1, 0]
-        dy = [0, -1, 0, 1]
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
         for i in range(4):
             xx = x + dx[i]
             yy = y + dy[i]
             if xx < 0 or xx >= m or yy < 0 or yy >= n:
                 continue
-            if visited[yy][xx]:
-                continue
-            if _map[yy][xx] == 1:
-                continue
+            if _map[xx][yy] == 1 and z == 0:
+                queue.append((xx, yy, 1))
+                visited[xx][yy][1] = visited[x][y][0] + 1
 
-            _map[yy][xx] = distance + 1
-            queue.append((xx, yy, distance + 1))
-            visited[yy][xx] = True
+            if _map[xx][yy] == 0 and visited[xx][yy][z] == 0:
+                queue.append((xx, yy, z))
+                visited[xx][yy][z] = visited[x][y][z] + 1
+
+    return -1
 
 
 input = sys.stdin.readline
@@ -33,17 +35,6 @@ if __name__ == "__main__":
     n, m = map(int, input().rstrip().split())
 
     _map = [list(map(int, input().rstrip())) for _ in range(n)]
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    wall_list = [(x, y) for x in range(m) for y in range(n) if _map[y][x] == 1]
-    wall_list.insert(0, (0, 0))
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
 
-    min_distance = 1415
-    for wall in wall_list:
-        _map_clone = copy.deepcopy(_map)
-        _map_clone[wall[1]][wall[0]] = 0
-        visited_clone = copy.deepcopy(visited)
-        bfs(_map_clone, visited_clone, 0, 0)
-        if _map_clone[n - 1][m - 1] != 0:
-            min_distance = min(min_distance, _map_clone[n - 1][m - 1])
-
-    print(min_distance if min_distance != 1415 else -1)
+    print(bfs(_map, visited, 0, 0, 0))
