924998acf4d8441d77773eb13c3990ae8813e591
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-11 21:05:24 +0900

    Greedy

--- BOJ/2294_동전 2.py
+++ BOJ/2294_동전 2.py
@@ -2,16 +2,16 @@ if __name__ == "__main__":
     n, k = map(int, input().split())
 
     coins = [int(input()) for _ in range(n)]
+    coins = list(set(coins))
+    coins.sort()
 
-    dp = [1 for _ in range(k + 1)]
-    dp[1] = 1
+    dp = [i for i in range(k + 1)]
 
-    for i in range(2, k + 1):
+    for i in range(min(coins), k + 1):
         for coin in coins:
-            if i % coin == 0:
-                dp[i] = dp[i // coin] + 1
+            dp[i] = min(dp[i], dp[i % coin] + i // coin)
 
-    if dp[k] == 0:
+    if dp[k] == k:
         print(-1)
     else:
         print(dp[k])
