85cf7922b38683047b3872c58e847f50d31b9755
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-12 00:17:47 +0900

    DP

--- BOJ/2294_동전 2.py
+++ BOJ/2294_동전 2.py
@@ -3,15 +3,16 @@ if __name__ == "__main__":
 
     coins = [int(input()) for _ in range(n)]
     coins = list(set(coins))
-    coins.sort()
+    coins = [coin for coin in coins if coin <= k]
 
-    dp = [i for i in range(k + 1)]
+    dp = [10001 for _ in range(k + 1)]
+    dp[0] = 0
 
     for i in range(min(coins), k + 1):
         for coin in coins:
-            dp[i] = min(dp[i], dp[i % coin] + i // coin)
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    if dp[k] == k:
+    if dp[k] == 10001:
         print(-1)
     else:
         print(dp[k])
