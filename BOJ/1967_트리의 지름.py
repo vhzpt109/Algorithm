5877dd1339e33d4ba3a7c111d771e3a34d1ab9dd
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-19 01:03:47 +0900

    Graph

--- BOJ/1967_트리의 지름.py
+++ BOJ/1967_트리의 지름.py
@@ -3,6 +3,15 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
+
+def dfs(x, wei):
+    for i in graph[x]:
+        a, b = i
+        if distance[a] == -1:
+            distance[a] = wei + b
+            dfs(a, wei + b)
+
+
 if __name__ == "__main__":
     n = int(input())
     graph = [[] for _ in range(n + 1)]
@@ -11,3 +20,14 @@ if __name__ == "__main__":
         a, b, c = map(int, input().split())
         graph[a].append([b, c])
         graph[b].append([a, c])
+
+    distance = [-1] * (n + 1)
+    distance[1] = 0
+    dfs(1, 0)
+
+    start = distance.index(max(distance))
+    distance = [-1] * (n + 1)
+    distance[start] = 0
+    dfs(start, 0)
+
+    print(max(distance))
