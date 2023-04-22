86a30e531dff95a6ae1a17f6dcba639529eef651
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-04-22 21:57:52 +0900

    Graph

--- BOJ/1167_트리의 지름.py
+++ BOJ/1167_트리의 지름.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 
-def dfs(x, weight_sum):
+def dfs(x, cost_sum):
     for start_node in graph[x]:
-        node, weight = start_node
+        node, cost = start_node
         if distance[node] == -1:
-            distance[node] = weight_sum + weight
-            dfs(node, weight_sum + weight)
+            distance[node] = cost_sum + cost
+            dfs(node, cost_sum + cost)
 
 
 if __name__ == "__main__":
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         input_int_list = list(map(int, input().split()))
         for j in range(1, len(input_int_list) - 1, 2):
             graph[i + 1].append([input_int_list[j], input_int_list[j + 1]])
+            graph[input_int_list[j]].append([i + 1, input_int_list[j + 1]])
 
     distance = [-1] * (n + 1)
     distance[1] = 0
