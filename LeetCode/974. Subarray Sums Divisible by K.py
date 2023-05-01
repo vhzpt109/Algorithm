305912d574e278666d0559fbaafdb49016e395b4
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-05-01 16:41:10 +0900

    Hash Table

--- LeetCode/974. Subarray Sums Divisible by K.py
+++ LeetCode/974. Subarray Sums Divisible by K.py
@@ -1,25 +1,22 @@
-from typing import List
-
-
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         cml_sums = []
         tmp_sum = 0
         for num in nums:
             tmp_sum += num
-        cml_sums.append(tmp_sum)
+            cml_sums.append(tmp_sum % k)
 
-        table = {}
+        hash_table = {}
         count = 0
-        table[0] = [-1]
+        hash_table[0] = [-1]
         for idx, cml_sum in enumerate(cml_sums):
-            target = cml_sum % k
-            if target in table:
-                count += 1
+            target = cml_sum
+            if target in hash_table:
+                count += len(hash_table[target])
 
-            if cml_sum not in table:
-                table[cml_sum] = [idx]
+            if target not in hash_table:
+                hash_table[target] = [idx]
             else:
-                table[cml_sum].append(idx)
+                hash_table[target].append(idx)
 
         return count
