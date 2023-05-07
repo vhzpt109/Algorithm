692d8af13452efacaa2817c925e23b986f467a9d
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-05-07 23:58:34 +0900

    Subarray Sum

--- LeetCode/209. Minimum Size Subarray Sum.py
+++ LeetCode/209. Minimum Size Subarray Sum.py
@@ -1,28 +1,28 @@
-import sys
 from typing import List
 
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        cml_sums = [0]
-        tmp_sum = 0
-        for num in nums:
-            tmp_sum += num
-            cml_sums.append(tmp_sum)
+        min_end = 0
+        min_len = len(nums)
+        w_sum = nums[0]
 
-        minSubLen = sys.maxsize
-        left = 0
-        right = 1
+        if sum(nums) < target: return 0
+        if nums[-1] >= target: return 1
 
-        while left <= right <= len(nums) and left <= len(nums):
-            if cml_sums[right] - cml_sums[left] >= target:
-                minSubLen = min(minSubLen, right - left)
-                left += 1
-                right += 1
-            elif cml_sums[right] - cml_sums[left] < target:
-                right += 1
+        for i in range(len(nums) - 1):
+            if i > 0:
+                w_sum -= nums[i - 1]
+            if i > min_end:
+                min_end = i
 
-        if minSubLen == sys.maxsize:
-            return -1
-        else:
-            return minSubLen
+            # find the min subarray
+            while min_end < len(nums):
+                if w_sum < target:
+                    min_end += 1
+                    if min_end == len(nums): break
+                    w_sum += nums[min_end]
+                if w_sum >= target:
+                    min_len = min(min_len, min_end - i + 1)
+                    break
+        return min_len
