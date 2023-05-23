8d1ea9a6803b560e19f34ab59ab99d4f988ce10f
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-05-23 21:36:22 +0900

    Minimum Coin Change

--- LeetCode/322. Coin Change.py
+++ LeetCode/322. Coin Change.py
@@ -3,24 +3,28 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [-1 for _ in range(amount + 1)]
-        idx = [i for i in range(1, amount + 1) if ]
-
-        for coin in coins:
-            dp[coin] = 1
+        dp = [0 for _ in range(amount + 1)]
+        coins = [coin for coin in coins if coin <= amount]
 
         for i in range(1, amount + 1):
+            min_cost = 2**32 - 1
             for coin in coins:
-                if i - coin < 0 or dp[i - coin] == -1:
+                if i < coin:
                     continue
-                dp[i] = min(dp[i], dp[i - coin])
-            dp[i] += 1
+                min_cost = min(min_cost, dp[i - coin] + 1)
+            if min_cost != 0:
+                dp[i] = min_cost
 
+        if dp[amount] == 2**32 - 1:
+            return -1
         return dp[amount]
 
 
 if __name__ == "__main__":
     obj = Solution()
-    coins = [1, 2, 5]
-    amount = 11
+    # coins = [1, 2, 5]
+    # coins = [2]
+    # amount = 11
+    coins = [1]
+    amount = 1
     print(obj.coinChange(coins, amount))
