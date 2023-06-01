2a03ea468fa017cdd3d18f5678ff8ce4832b01b6
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-06-01 23:39:12 +0900

    Clone Graph

---
+++ LeetCode/133. Clone Graph.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+
+        queue = deque([node])
+        clone_graph = {node.val: Node(node.val, [])}
+
+        while queue:
+            current_node = queue.popleft()
+
+            for connected_node in current_node.neighbors:
+                if connected_node.val not in clone_graph:
+                    clone_graph[connected_node.val] = Node(connected_node.val, [])
+                    queue.append(connected_node)
+
+        return clone_graph[node.val]
