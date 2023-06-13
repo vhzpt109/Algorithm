25e93da49f960888e0b749fdcc22243bcdfdbe78
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-06-13 18:45:42 +0900

    Gas Station

--- LeetCode/134. Gas Station.py
+++ LeetCode/134. Gas Station.py
@@ -15,23 +15,26 @@ class Solution:
             for i in range(1, length + 1):
                 g -= cost[idx + i]
                 if g < 0:
-                    idx = idx + i
+                    idx = idx + i + 1
                     pass_flag = False
                     break
                 g += gas[idx + i]
             if pass_flag:
-                return idx + 1
-            idx += 1
+                return idx
 
         return -1
 
 
 if __name__ == "__main__":
     obj = Solution()
-    gas = [1, 2, 3, 4, 5]
-    cost = [3, 4, 5, 1, 2]
+    # gas = [1, 2, 3, 4, 5]
+    # cost = [3, 4, 5, 1, 2]
     # gas = [2, 3, 4]
     # cost = [3, 4, 3]
     # gas = [3, 3, 4]
     # cost = [3, 4, 4]
+    # gas = [5, 1, 2, 3, 4]
+    # cost = [4, 4, 1, 5, 1]
+    gas = [5, 8, 2, 8]
+    cost = [6, 5, 6, 6]
     print(obj.canCompleteCircuit(gas, cost))
