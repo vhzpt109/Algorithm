3c9a4a0d988c470eb7ef45a9df58ee7e547e4b7e
vhzpt109 <vhzpt109@naver.com>
2023-07-12 18:12:31 +0900

    List with Random Pointer

---
+++ LeetCode/138. Copy List with Random Pointer.py
@@ -0,0 +1,37 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+
+        if head is None:
+            return head
+
+        node_hash_map = {}
+
+        dummy_head = Node(-1)
+        deepcopy_node = dummy_head
+
+        current_node = head
+        while current_node:
+            val = current_node.val
+            temp_node = Node(x=val)
+
+            deepcopy_node.next = temp_node
+            node_hash_map[current_node] = temp_node
+
+            deepcopy_node = deepcopy_node.next
+            current_node = current_node.next
+
+        current_node = head
+        while current_node:
