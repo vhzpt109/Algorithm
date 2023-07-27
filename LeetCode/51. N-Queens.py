60e65c51cce1227021697d151aa376114510be8e
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-07-27 23:45:59 +0900

    N-Queen

--- LeetCode/51. N-Queens.py
+++ LeetCode/51. N-Queens.py
@@ -3,12 +3,14 @@ from typing import List
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [[False] * n for _ in range(n)]
+        board = [['.'] * n for _ in range(n)]
         self.backtracking(board, 0)
+
+        return board
 
     def is_available_location(self, board, x, y, n):
         for i_y in reversed(range(y)):
-            if board[i_y][x]:
+            if board[i_y][x] == 'Q':
                 return False
 
         lu = [x, y]
@@ -17,12 +19,12 @@ class Solution:
             if 0 < lu[0] and 0 < lu[1]:
                 lu[0] -= 1
                 lu[1] -= 1
-                if board[lu[1]][lu[0]]:
+                if board[lu[1]][lu[0]] == 'Q':
                     return False
             if ru[0] < n - 1 and 0 < ru[1]:
                 ru[0] += 1
                 ru[1] -= 1
-                if board[ru[1]][ru[0]]:
+                if board[ru[1]][ru[0]] == 'Q':
                     return False
         return True
 
@@ -30,10 +32,11 @@ class Solution:
         for x in range(len(board[y])):
             if self.is_available_location(board, x, y, len(board)):
                 if y == len(board) - 1:
-                    return
-                board[y][x] = True
-                self.backtracking(board, y + 1)
-                board[y][x] = False
+                    return True
+                board[y][x] = 'Q'
+                if self.backtracking(board, y + 1):
+                    return True
+                board[y][x] = '.'
 
 
 if __name__ == "__main__":
