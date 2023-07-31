565e9f5e2d4126b55eb8c67dbc8bcf09d2388ebc
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-07-31 23:01:59 +0900

    Palindrome Partitioning

--- LeetCode/131. Palindrome Partitioning.py
+++ LeetCode/131. Palindrome Partitioning.py
@@ -3,30 +3,38 @@ from typing import List
 
 class Solution:
     def __init__(self):
+        self.s = None
         self.results = []
 
     def partition(self, s: str) -> List[List[str]]:
+        self.s = s
         for i in range(len(s)):
-            self.backtracking(s[i:i + 1], [])
-            self.backtracking(s[i + 1:], [])
+            self.backtracking(0, i, [])
 
         return self.results
 
-    def backtracking(self, s, result):
-        if self.isPalindrome(s):
-            result.append(s.copy())
-        else:
+    def backtracking(self, begin, last, letters):
+        if not self.isPalindrome(self.s[begin:last + 1]):
             return
 
-        for i in range(len(s)):
-            self.backtracking(s[i: i + 1], result)
-            self.backtracking(s[i + 1:], result)
+        letters.append(self.s[begin:last + 1])
+
+        if len(self.s) == last + 1:
+            self.results.append(letters.copy())
+            letters.pop()
+            return
+
+        for i in range(last + 1, len(self.s)):
+            self.backtracking(last + 1, i, letters)
+
+        letters.pop()
+        return
 
     def isPalindrome(self, s):
-        return s == s[-1::]
+        return s == s[::-1]
 
 
-if __name__ == "__mian__":
+if __name__ == "__main__":
     obj = Solution()
     s = "aabb"
     print(obj.partition(s))
