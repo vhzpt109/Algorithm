5773f46a04856f833220417af6bc152242bd507a
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2023-08-03 23:11:51 +0900

    Longest Palindromic Substring

--- LeetCode/5. Longest Palindromic Substring.py
+++ LeetCode/5. Longest Palindromic Substring.py
@@ -16,10 +16,32 @@ class Solution:
             row = 0
             col = i
             while col < str_length:
-                
+                start = s[row]
+                end = s[col]
+                prev_count = dp[row + 1][col - 1]
+                if start == end and prev_count != 0:
+                    dp[row][col] = prev_count + 2
+                row += 1
+                col += 1
+
+        max_length = 0
+        start = 0
+        end = 0
+        for row in range(str_length):
+            for col in range(str_length):
+                current_length = dp[row][col]
+                if max_length < current_length:
+                    max_length = current_length
+                    start = row
+                    end = col
+
+        sub_str = s[start:end + 1]
+
+        return sub_str
 
 
 if __name__ == "__main__":
     obj = Solution()
-    s = "aabb"
+    # s = "aabb"
+    s = "baabc"
     print(obj.longestPalindrome(s))
